1e8e2e1d4e93548f5e4057bafa493c6b6de9d382
pilotlohith <pilotlohith@gmail.com>
2021-11-29 21:54:11 +0530

    added credential support

--- src/main.py
+++ src/main.py
@@ -1,4 +1,25 @@
 def login():
+    
     pass 
 def log (message):
+    pass
+def login():
+    pass 
+def log (message):
+    pass
+this is a change adding in rel1.0
+    pass
+this is to check for conflict 1.0
+    pass
+this is to check for conflict 1.1
+    pass
+this is to check for conflict 1.1
+    pass
+this is to check for second conflict
+   pass
+ def user_profile_pic():
+     pass 
+def reset password 
+     pass
+    forget to add
     pass
